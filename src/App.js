1faccbc24463657bfb0a363b55ed8e586d5c0068
fidelis <developer@cayor.tech>
2022-11-15 16:11:36 +0000

    responsive css added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import formimg from "./images/form-img.jpg";
 import "./App.scss";
+import {React,  useState} from "react";
 
 function App() {
+  const [error, setError] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  });
+  
+// Function to save data to local storage 
+  const saveform = (event) => {
+    event.preventDefault();
+    if (validateind()) {
+     localStorage.setItem('form', JSON.stringify(user));
+    }
+    else{
+      console.log(user);
+    }
+  }
+
+
+// Function to get changes made on form
+  const handleindChange = (event) => {
+    setUser({ ...user, [event.target.id]: event.target.value });
+  };
+
+// Function to validate form
+  const validateind = () => {
+    let temp = []
+    temp.name = user.name ? "" : "This field is required"
+    if (!user.email) {
+      temp.email = "This field is required"
+    } else if (!/\S+@\S+\.\S+/.test(user.email)) {
+      temp.email = 'Email address is invalid';
+    }
+    if (!user.phone) {
+      temp.phone = "This field is required"
+    } else if (user.phone.length < 7) {
+      temp.phone = 'Phone number should be at least 7 digits';
+    }
+    temp.password = user.password ? "" : "This field is required"
+    if (!user.confirmPassword) {
+      temp.confirmPassword = "This field is required"
+    } else if (user.confirmPassword !== user.password) {
+      temp.confirmPassword = 'Passwords dont match';
+    }
+    setError({ ...temp })
+    return Object.values(temp).every(x => x == "")
+  }
+
   return (
     <div className="App">
       <div className="flex-container">
         <div className="img-container">
           <div className="overlay">
             {" "}
-            <span className="img-text">
+            <span className="text">
               <p>
                 The on-demand, all inclusive. <br /> healthcare management
                 ecosystem{" "}
@@ -17,75 +68,94 @@ function App() {
           </div>
           <img className="form-img" src={formimg} alt="" />
         </div>
-
         <div className="form-container">
-          <div className="form-content">
-            <div className="form-header">
+            <form className="forms" action="POST" onSubmit={saveform}>
+            <div className="title">
             <span >Create an Account</span>
             </div>
-            <form className="forms" action="">
-              <label className="label" for="fname">
-                First name:
+              <div className="input-field">
+                {/* Name Input */}
+              <label for="fname">
+                Name <span className="error">{error.name}*</span>
               </label>
-              <br />
+            
+              <span className="space"></span>
               <input
-                className="form-input"
                 type="text"
-                id="fname"
-                name="fname"
-              />
-              <br />
-              <label className="label" for="fname">
-                Work Email:
+                id="name"
+                name="name"
+                value={user.name}
+                onChange={handleindChange}
+                placeholder="Your Name"
+              /></div>
+              <div className="input-field">
+              {/* Email Input */}
+              <label for="fname">
+                Work Email <span className="error">{error.email}*</span>
               </label>
-              <br />
+           
               <input
-                className="form-input"
+                
                 type="text"
-                id="fname"
-                name="fname"
-              />
-              <br />
-              <label className="label" for="fname">
-                Telephone :
+                id="email"
+                name="email"
+                value={user.email}
+                onChange={handleindChange}
+                placeholder="Your Email"
+              /></div>
+             {/* Phone Input */}
+             <div className="input-field">
+              <label for="fname">
+                Telephone <span className="error">{error.phone}*</span>
               </label>
-              <br />
+          
               <input
-                className="form-input"
-                type="text"
-                id="fname"
-                name="fname"
-              />
-              <br />
-              <label className="label" for="fname">
-                Password :
+                
+                type="Number"
+                id="phone"
+                name="phone"
+                value={user.phone}
+                onChange={handleindChange}
+                placeholder="+220 2366073"
+              /></div>
+              {/* Password Input */}
+              <div className="input-field">
+              <label for="fname">
+                Password <span className="error">{error.password}*</span>
               </label>
-              <br />
+            
               <input
-                className="form-input"
                 type="text"
-                id="fname"
-                name="fname"
-              />
-              <br />
-              <label className="label" for="fname">
-                Confirm Password :
+                id="password"
+                name="password"
+                value={user.password}
+                onChange={handleindChange}
+                placeholder="Your Password"
+              /> </div>
+              <div className="input-field">
+              <label for="fname">
+                Confirm Password <span className="error">{error.confirmPassword}*</span>
               </label>
-              <br />
+             
               <input
-                className="form-input"
                 type="text"
-                id="fname"
-                name="fname"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={user.confirmPassword}
+                onChange={handleindChange}
+                placeholder="Confirm your Password"
               />
-              <br />
-              <button className="button" type="button">
-                Click Me!
+              </div>
+              <div className="input-field">
+              <button onClick={saveform} className="button" type="button">
+                Agree & Signup
               </button>
-              <br />
-              <span className="form-signin"> Already have an account ? <a href="#">Sign-in</a></span>
+              </div>
+              <div className="input-field">
+              <span className="signin"> Already have an account ? <a href="#">Sign-in</a></span>
+              </div>
             </form>
-          </div>
+          
         </div>
       </div>
     </div>
